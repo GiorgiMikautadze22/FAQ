9085f25c7ba074640ce8bc2e34ce0604148f2e82
creatorm22 <mikautadze.giorgi2223@gmail.com>
2023-07-09 14:36:16 +0400

    added responsive and js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("test");
+let bold = document.getElementById("bold");
+let arrow = document.getElementById("arrow");
+let hide = document.getElementById("hide");
+
+bold.addEventListener("click", () => {
+  if (bold.style.fontWeight === "normal") {
+    bold.style.fontWeight = "bold";
+  } else {
+    bold.style.fontWeight = "normal";
+  }
+
+  if (hide.style.display === "none") {
+    hide.style.display = "block";
+  } else {
+    hide.style.display = "none";
+  }
+  arrow.classList.add("arrow");
+  // if ((bold.style.fontWeight = "bold") && (hide.style.display = "block")) {
+  //   arrow.classList.add("arrow");
+  // } else {
+  //   arrow.classList.add("arrow");
+  // }
+});
